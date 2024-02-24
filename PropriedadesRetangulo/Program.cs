8d78f5d8f463facd78b9d9833014512a1ee98722
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-02-24 18:56:49 -0300

    Resolvido sem OO

--- PropriedadesRetangulo/Program.cs
+++ PropriedadesRetangulo/Program.cs
@@ -6,7 +6,20 @@ namespace PropriedadesRetangulo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Propriedades de um retângulo");
+            Console.Write("Digite a largura: ");
+            double largura = double.Parse(Console.ReadLine());
+            Console.Write("Digite a altura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double area = altura * largura;
+            double perimetro = 2 * altura + 2 * largura;
+            double diagonal = Math.Sqrt(altura * altura + largura * largura);
+
+            Console.WriteLine("\nEsse retângulo tem" +
+                "\nArea: " + area +
+                "\nPerimetro: " + perimetro +
+                "\nDiagonal: " + diagonal);
         }
     }
 }
