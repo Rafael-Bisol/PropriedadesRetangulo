f20342fb871aa767c9e835016fce76b3961a44b6
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-02-24 19:02:50 -0300

    Resolvido com OO

--- PropriedadesRetangulo/Program.cs
+++ PropriedadesRetangulo/Program.cs
@@ -6,20 +6,19 @@ namespace PropriedadesRetangulo
     {
         static void Main(string[] args)
         {
+            Retangulo retangulo1 = new Retangulo();
+            
             Console.WriteLine("Propriedades de um retângulo");
             Console.Write("Digite a largura: ");
-            double largura = double.Parse(Console.ReadLine());
+            retangulo1.largura = double.Parse(Console.ReadLine());
             Console.Write("Digite a altura: ");
-            double altura = double.Parse(Console.ReadLine());
+            retangulo1.altura = double.Parse(Console.ReadLine());
 
-            double area = altura * largura;
-            double perimetro = 2 * altura + 2 * largura;
-            double diagonal = Math.Sqrt(altura * altura + largura * largura);
 
             Console.WriteLine("\nEsse retângulo tem" +
-                "\nArea: " + area +
-                "\nPerimetro: " + perimetro +
-                "\nDiagonal: " + diagonal);
+                "\nArea: " + retangulo1.CalcularArea() +
+                "\nPerimetro: " + retangulo1.CalcularPerimetro() +
+                "\nDiagonal: " + retangulo1.CalcularDiagonal());
         }
     }
 }
---
+++ PropriedadesRetangulo/Retangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PropriedadesRetangulo
+{
+    class Retangulo
+    {
+        public double largura, altura;
+
+        public double CalcularArea()
+        {
+            return largura * altura;
+        }
+
+        public double CalcularPerimetro()
+        {
+            return 2 * largura + 2 * altura;
+        }
+
+        public double CalcularDiagonal()
+        {
+            return Math.Sqrt(largura * largura + altura * altura);
+        }
+    }
+}
